960ea3263d1a9beb2255312c893d0927a34122c4
Vic <victordjohnson@gmail.com>
2018-11-19 12:30:04 -0600

    removed the random functions in the Enemy component as we will generate that within the Helper file

--- src/Components/Enemy.js
+++ src/Components/Enemy.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { EnemySC } from './StyledComponents';
 
-const randomEn = () => Math.floor(Math.random() * 3);
-
-const Enemy = () => <EnemySC color={randomEn()} />;
+const Enemy = () => <EnemySC />;
 
 export default Enemy;
