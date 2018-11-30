dad235883b2952beeb372bc27aa1489f223c1b87
Vic <victordjohnson@gmail.com>
2018-11-30 15:46:54 -0600

    converted the Ship component from a class component into a stateless one

--- src/Components/Ship.js
+++ src/Components/Ship.js
@@ -3,25 +3,16 @@ import React from 'react';
 import { ShipSC, ShipContainerSC, FirePoseSC } from './StyledComponents';
 import { withContext } from '../Context';
 
-class Ship extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Ship = props => {
+  const { shipX, rVelocity, lVelocity } = props.context;
+  const rotateDeg = (rVelocity + lVelocity) * 2.5;
 
-  // componentDidMount() {
-  //   setInterval(() => this.setState({ flame: !this.state.flame }), 200);
-  // }
-
-  render() {
-    const { shipX, rVelocity, lVelocity } = this.props.context;
-    const rotateDeg = (rVelocity + lVelocity) * 2.5;
-    return (
-      <ShipContainerSC style={{ left: shipX }} rotate={rotateDeg}>
-        <ShipSC />
-        <FirePoseSC />
-      </ShipContainerSC>
-    );
-  }
-}
+  return (
+    <ShipContainerSC style={{ left: shipX }} rotate={rotateDeg}>
+      <ShipSC />
+      <FirePoseSC />
+    </ShipContainerSC>
+  );
+};
 
 export default withContext(Ship);
