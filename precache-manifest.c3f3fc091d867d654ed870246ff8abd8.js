b7612b89e2c3b118e89475330db58a7110dc33f8
Vic <victordjohnson@gmail.com>
2019-01-19 18:43:47 -0600

    Updates

---
+++ precache-manifest.c3f3fc091d867d654ed870246ff8abd8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb267ab898220d2a266e4433064827c0",
+    "url": "/react-space-barrage/static/media/enemy2.bb267ab8.svg"
+  },
+  {
+    "revision": "182995b59ef7f185e192",
+    "url": "/react-space-barrage/static/css/main.dd47c2c6.chunk.css"
+  },
+  {
+    "revision": "03cf5d1c058d87a4fc8c",
+    "url": "/react-space-barrage/static/js/1.03cf5d1c.chunk.js"
+  },
+  {
+    "revision": "7089173d570b2c00a707",
+    "url": "/react-space-barrage/static/js/runtime~main.7089173d.js"
+  },
+  {
+    "revision": "eefd8f35cba411cc9915361a2cf1c00f",
+    "url": "/react-space-barrage/static/media/enemy1.eefd8f35.svg"
+  },
+  {
+    "revision": "182995b59ef7f185e192",
+    "url": "/react-space-barrage/static/js/main.182995b5.chunk.js"
+  },
+  {
+    "revision": "e7e49935ff74f0adac34374bb172c8ee",
+    "url": "/react-space-barrage/static/media/enemy3.e7e49935.svg"
+  },
+  {
+    "revision": "7371d9833ef03843ce394990ada71626",
+    "url": "/react-space-barrage/static/media/rocket.7371d983.svg"
+  },
+  {
+    "revision": "58ef52efe5e4aef0b5a30cc937fe51f8",
+    "url": "/react-space-barrage/static/media/fire.58ef52ef.svg"
+  },
+  {
+    "revision": "ccb1dfce06ad3883f8e3b5ae011aa795",
+    "url": "/react-space-barrage/static/media/PressStart2P-Regular.ccb1dfce.ttf"
+  },
+  {
+    "revision": "0ccd46d561cff538b3a976da1a2768a4",
+    "url": "/react-space-barrage/index.html"
+  }
+];
