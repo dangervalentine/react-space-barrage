bc9ead385e1e0aa0c9a00f79ab8bacc7e4cf5d04
Vic <victordjohnson@gmail.com>
2019-01-19 18:36:32 -0600

    made a branch of this project that uses create-react-app for the entire project. want to play around with deploying the application to gh pages and netlify

---
+++ build/precache-manifest.3772780e991ddece67eba617883fbd1b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb267ab898220d2a266e4433064827c0",
+    "url": "/static/media/enemy2.bb267ab8.svg"
+  },
+  {
+    "revision": "d776b9fe73e3b61c6465",
+    "url": "/static/css/main.0dbe97c0.chunk.css"
+  },
+  {
+    "revision": "33da51967a70fec7b20f",
+    "url": "/static/js/1.33da5196.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eefd8f35cba411cc9915361a2cf1c00f",
+    "url": "/static/media/enemy1.eefd8f35.svg"
+  },
+  {
+    "revision": "d776b9fe73e3b61c6465",
+    "url": "/static/js/main.d776b9fe.chunk.js"
+  },
+  {
+    "revision": "e7e49935ff74f0adac34374bb172c8ee",
+    "url": "/static/media/enemy3.e7e49935.svg"
+  },
+  {
+    "revision": "7371d9833ef03843ce394990ada71626",
+    "url": "/static/media/rocket.7371d983.svg"
+  },
+  {
+    "revision": "58ef52efe5e4aef0b5a30cc937fe51f8",
+    "url": "/static/media/fire.58ef52ef.svg"
+  },
+  {
+    "revision": "ccb1dfce06ad3883f8e3b5ae011aa795",
+    "url": "/static/media/PressStart2P-Regular.ccb1dfce.ttf"
+  },
+  {
+    "revision": "30d7d15ced23cc65885d7ec7445b3b4a",
+    "url": "/index.html"
+  }
+];
